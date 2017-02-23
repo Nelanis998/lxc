bc760087619367b97d9e3cd33fc4a5d34abecd0d
Nelanis998 <e12.kiss.bianka.maria@palfy-vizugyi.hu>
2017-02-23 09:15:02 +0100

    Update kontener.sh

--- bin/kontener.sh
+++ bin/kontener.sh
@@ -2,5 +2,5 @@
 lxc init ubuntu:16.04 bianka
 lxc start bianka
 sleep 15
-lxc exec bianka --apt update -y
-lxc exec bianka --apt upgrade -y
+lxc exec bianka -- apt update -y
+lxc exec bianka -- apt upgrade -y
